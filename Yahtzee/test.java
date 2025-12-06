8ffce58343ae0e831b063a06aacd901a98a1fd59
jbollinger01 <jbollinger01@icloud.com>
2025-12-06 16:39:27 -0600

    Add files via upload

---
+++ Yahtzee/test.java
@@ -0,0 +1,24 @@
+package yatzee;
+
+public class test {
+
+//	public static void main(String[] args) {
+
+
+//		int[] testRolls = {1,2,3,4,5};
+//		int[] upperScore = {1,2,3,4,5,6};
+		
+//		Player p = new Player("test");
+//		p.setRolls(testRolls);
+//		p.setUpperSection(upperScore);
+		
+		
+//		p.scoreSingleNumber(6);
+//		System.out.println(p.scoreUpper());
+//		p.roll();
+		
+//		p.test();
+		
+//	}
+
+}
